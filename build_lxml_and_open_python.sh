ffd00066c543f3c0e0f91fb52bf72f2d94613e12
Abraham Polk <21989062+abepolk@users.noreply.github.com>
2024-09-26 19:42:09 -0400

    Create build_lxml_and_open_python.sh

---
+++ build_lxml_and_open_python.sh
@@ -0,0 +1,20 @@
+sudo apt install autoconf build-essential gcc git libtool pkg-config python3-dev python3-venv -y
+python3 -m venv venv
+source venv/bin/activate
+git clone https://gitlab.gnome.org/GNOME/libxml2.git
+cd libxml2/
+./autogen.sh
+make
+sudo make install
+cd ..
+git clone https://gitlab.gnome.org/GNOME/libxslt.git
+cd libxslt/
+./autogen.sh
+make
+sudo make install
+cd ..
+git clone https://github.com/lxml/lxml.git
+cd lxml/
+pip3 install -r requirements.txt
+python3 setup.py build_ext -i --with-cython --with-xml2-config=$HOME/libxml2/xml2-config --with-xslt-config=$HOME/libxslt/xslt-config
+PYTHONPATH=src LD_LIBRARY_PATH=/usr/local/lib python3
