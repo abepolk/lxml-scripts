b53b6ae6653866fed3c94d6bd881ec76f3a6de4f
Abe Polk <21989062+abepolk@users.noreply.github.com>
2025-07-06 15:57:35 -0400

    Revert back to before working on Terraform files, as they have been moved to the separate lxml-env repo

--- terraform_destroy_wrapper.sh
+++
@@ -1,3 +0,0 @@
-# The second variable is the storage location pulled from metadata
-# TODO Check the ability of this to be interactive  if run in a subshell
-terraform destroy -var="project_id=$(gcloud config get-value project)"
